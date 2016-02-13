0eb615cd41d0a94b52f9f6c3f4e3c93ef01464f1
Shepelev <shepelev.dim@gmail.com>
2016-02-13 18:44:52 +0200

    Implementation of Router and Application->run()

--- framework/Application.php
+++ framework/Application.php
@@ -1,7 +1,31 @@
 <?php
 
 namespace Framework;
+use Framework\Router\Router;
+
+/**
+ * Class Application
+ * @package Framework
+ */
 
 class Application {
 
+    /**
+     *  The method starts the app
+     */
+    public function run(){
+
+        $router = new Router( include( '../app/config/routes.php' ));
+        $route =  $router->parseRoute( $_SERVER[ 'REQUEST_URI' ] );
+
+        if( !empty( $route )){
+            echo '<pre>';
+            print_r( $route );
+        }
+        else {
+            echo 'The page hasn\' found. 404 ';
+        }
+
+
+    }
 }
