9ca56d165f4aefda18996063afdd801c26ee2034
Shepelev <shepelev.dim@gmail.com>
2016-03-27 11:45:46 +0300

    User's table

--- app/db.sql
+++ app/db.sql
@@ -32,3 +32,16 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR (50) NOT NULL,
+  `password` VARCHAR (50) NOT NULL,
+  `role` VARCHAR (50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
